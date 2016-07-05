f38a575c7c02a6350ae311bbca6b60d196fa83cd
Coder_feng <835762882@qq.com>
2016-07-05 15:49:14 +0800

    image animation
    image animation

--- 图片动画/ViewController.m
+++ 图片动画/ViewController.m
@@ -16,7 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional  setup after loading the view, typically from a nib.
+    
+    NSArray *images = [NSArray arrayWithObjects:[UIImage imageNamed:@"01.tiff"], [UIImage imageNamed:@"02.tiff"], [UIImage imageNamed:@"03.tiff"], [UIImage imageNamed:@"04.tiff"], nil];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(200, 100, 100, 100)];
+    imageView.animationImages = images;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.animationDuration = 0.5;
+    imageView.animationRepeatCount = 0;
+    [imageView startAnimating];
+    [self.view addSubview:imageView];
+    
+    [UIView animateWithDuration:3 animations:^{
+        CGRect frame = imageView.frame;
+        frame.origin.x = 0;
+        imageView.frame = frame;
+    } completion:^(BOOL finished) {
+        [imageView removeFromSuperview];
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
